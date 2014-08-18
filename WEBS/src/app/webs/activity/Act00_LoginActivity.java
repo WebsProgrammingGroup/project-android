f44e3f1d71ed15e2c5d367885666886c31f3d582
Nam gunbaek <rjsqor@gmail.com>
2014-08-18 15:46:52 +0900

    회원가입 액티비티까지 폼 완성
    회원가입 액티비티까지 폼 완성

---
+++ WEBS/src/app/webs/activity/Act00_LoginActivity.java
@@ -0,0 +1,46 @@
+package app.webs.activity;
+
+import com.beardedhen.androidbootstrap.*;
+import com.webs.app.*;
+import com.webs.app.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+
+public class Act00_LoginActivity extends Activity implements OnClickListener{
+	private BootstrapButton JoinBtn;
+	private BootstrapButton LoginBtn;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.act00_login);
+		
+		JoinBtn = (BootstrapButton)findViewById(R.id.a00_btn_join);
+		LoginBtn = (BootstrapButton)findViewById(R.id.a00_btn_login);
+		
+		JoinBtn.setOnClickListener(this);
+		LoginBtn.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		Intent i;
+		switch (v.getId()) {
+		case R.id.a00_btn_login:
+			break;
+		case R.id.a00_btn_join:
+			i = new Intent(this, Act01_JoinUs.class);
+			startActivity(i);
+			overridePendingTransition(R.anim.viewin3, R.anim.viewout3);
+			break;
+		default:
+			break;
+		}
+	}
+
+}
---
+++ WEBS/src/app/webs/activity/StaticVar.java
@@ -0,0 +1,12 @@
+package app.webs.activity;
+
+import java.util.*;
+
+import android.content.*;
+import app.webs.service.*;
+
+public class StaticVar {
+	public static Boolean isBound = false;
+	public static ServiceConnection mServiceConncetion = null;
+	public static PushService mService = null;
+}
